bb7351483111a6662ddc7aac3ac6871c6587b4b5
Simon Drake <simondrake1990@gmail.com>
2021-11-25 11:03:08 +0000

    Add tanks table

---
+++ migrations/000009_create_tanks_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS "tanks" (
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "make" VARCHAR(40) DEFAULT '',
+  "model" VARCHAR(40) DEFAULT '',
+  "name" VARCHAR(40) DEFAULT '',
+  "location" VARCHAR(40) DEFAULT '',
+  "capacity_measurement" VARCHAR(10) DEFAULT '',
+  "capacity" FLOAT DEFAULT NULL,
+  "description" VARCHAR(255) DEFAULT '',
+  "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  "updated_at" TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
